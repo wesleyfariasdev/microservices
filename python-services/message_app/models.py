9c44ff841de13ef9ea15cdbbe765bec4371925c5
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-03-20 03:52:10 -0300

    refactor: permite que o campo fique em branco

--- python-services/message_app/models.py
+++ python-services/message_app/models.py
@@ -4,7 +4,7 @@ class Message(models.Model):
     id = models.AutoField(primary_key=True) 
     message = models.CharField(max_length=50)
     date_time_received = models.DateTimeField(auto_now_add=True)
-    date_time_sending = models.DateTimeField()
+    date_time_sending = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f'Comunicação: {self.id}'
