c8385a6755f7df8423693dbba574b3ee7a87dd76
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-03-20 03:40:51 -0300

    feat: adiciona model Message para envio e recebimento de mensagem

--- python-services/message_app/models.py
+++ python-services/message_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model): 
+    id = models.AutoField(primary_key=True) 
+    message = models.CharField(max_length=50)
+    date_time_received = models.DateTimeField(auto_now_add=True)
+    date_time_sending = models.DateTimeField()
+
+    def __str__(self):
+        return f'Comunicação: {self.id}'
